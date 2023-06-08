32555541902ea49a6b4368a1d3627ca0f9869325
Remerson Monteiro <kairemerson@gmail.com>
2023-06-08 15:46:05 -0300

    desafio finalizado

--- src/index.ts
+++ src/index.ts
@@ -40,4 +40,38 @@ app.listen(PORT, () => {
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
 	
+	const readline = require("readline")
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	})
+
+	type alunosT = {
+		nome: string,
+		nota: number
+	}
+
+	let alunos:alunosT[] = []
+	let quantidade = 0
+
+	rl.question("quantidade de alunos?\n", (qnt: number)=>{
+		quantidade = qnt
+		
+		rl.setPrompt("insira o 'nome' espaço 'nota' ex: Ana 10\n")
+		rl.prompt()
+		rl.on("line", (nome: string)=>{
+			let arr = nome.split(" ")
+			alunos.push({nome: arr[0], nota: Number(arr[1])})
+			if(alunos.length == quantidade){
+				rl.close()
+			}
+		})
+	})
+	rl.on("close",()=>{
+		const maxNota = alunos.reduce((prev, current)=>{
+			return prev.nota > current.nota ? prev : current
+		})
+		console.log(`Maior nota foi de ${maxNota.nome} e a nota foi ${maxNota.nota}`);
+		
+	})
 });
